f18e5f42dd836f2d44a2ecff6997416dd0b9726d
Ben Smith <bsmith@apl.washington.edu>
2018-04-18 15:37:07 -0700

    ATL06_to_ATL11.py: fixed an indexing problem.

--- ATL06_to_ATL11.py
+++ ATL06_to_ATL11.py
@@ -7,6 +7,7 @@ from ATL11 import ATL11_data, ATL11_point, ATL11_defaults
 import matplotlib.pyplot as plt 
 import time
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 #from poly_ref_surf import poly_ref_surf
 
@@ -15,9 +16,10 @@ def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None):
     # read in the ATL06 data
     D6=ATL06_data(filename=ATL06_files, beam_pair=beam_pair) # two cols (two segs)
     # reoder data rows from D6 by cycle
+    this_index=np.argsort(D6.cycle[:,0],axis=0)
     for field in D6.list_of_fields:
         tmp=getattr(D6,field)
-        setattr(D6,field,tmp[np.argsort(D6.cycle[:,0],axis=0),:])
+        setattr(D6,field,tmp[this_index, :])
         
     if seg_x_centers is None:
         # NO: select every nth center        
@@ -25,13 +27,14 @@ def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None):
     for seg_x_center in seg_x_centers:
         print('seg_x_center',seg_x_center)
         # section 5.1.1 ?
-        D6_sub=D6.subset(np.any(np.abs(D6.x_atc-seg_x_center) < params_11.L_search_AT, axis=1)) # len 144 = 12 xlocs, by 12 cycles where ylocs are diff for each cycle, xlocs the same for all cycles.
+        D6_sub=D6.subset(np.any(np.abs(D6.x_atc-seg_x_center) < params_11.L_search_AT, axis=1), by_row=True) # len 144 = 12 xlocs, by 12 cycles where ylocs are diff for each cycle, xlocs the same for all cycles.
         print('D6 sub shape',D6_sub.x_atc.shape)
         #2a. define representative x and y values for the pairs
         pair_data=D6_sub.get_pairs()   # this might go, similar to D6_sub
 
         P11=ATL11_point(N_pairs=len(pair_data.x), x_atc_ctr=seg_x_center, y_atc_ctr=None, track_azimuth=np.nanmedian(D6_sub.seg_azimuth.ravel()) )
-        # step 2: select pairs, based on reasonable slopes
+        P11.DOPLOT=True
+       # step 2: select pairs, based on reasonable slopes
         P11.select_ATL06_pairs(D6_sub, pair_data, params_11)
         P11.select_y_center(D6_sub, pair_data, params_11)
                 
@@ -40,7 +43,7 @@ def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None):
         P11.find_reference_surface(D6_sub, params_11)
         
         P11.corr_heights_other_cycles(D6_sub, params_11)
-
+        
     return
   
 def regress_to(D, out_field_names, in_field_names, in_field_pt):
