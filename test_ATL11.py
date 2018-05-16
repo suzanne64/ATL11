1a77d854cdd0e00aa026249fe5be4b4af4bb3fc9
Ben Smith <bsmith@apl.washington.edu>
2018-05-16 14:10:29 -0700

    test_ATL11.py: added more commented text options

--- test_ATL11.py
+++ test_ATL11.py
@@ -7,6 +7,7 @@ Created on Wed Oct 25 16:24:11 2017
 from glob import glob
 import numpy as np
 from ATL06_to_ATL11 import fit_ATL11
+from ATL11 import ATL11_data
 #import sys
  
 np.seterr(invalid='ignore')
@@ -19,7 +20,8 @@ np.seterr(invalid='ignore')
 #filenames=glob('/Volumes/ice1/ben/sdt/ATLxx_example/PIG_Collab_v13A/ATL06/run_1/rep_*/Track_462_D3.h5')
 filenames=glob('/Volumes/ice1/ben/sdt/ATLxx_example/PIG_Collab_v13B_NoFirn_NoDz/ATL06/run_1/rep_*/Track_462_D3.h5') 
 #x_ctr=33044510.0
-x_ctr=33046250.0
+#x_ctr=33046250.0
+x_ctr=33047260
 #x_ctr=33046270.0
 
 #filenames=glob('/Volumes/ice1/ben/sdt/ATLxx_example/PIG_Collab_v13B_NoFirn_NoDz/ATL06/run_1/rep_*/Track_469_D3.h5') 
@@ -29,4 +31,7 @@ x_ctr=33046250.0
 #filenames=filenames[0:3] 
 # NEED TO TRY WITH THESE FILES AND PAIR=1
 
-P11=fit_ATL11(filenames, beam_pair=3, seg_x_centers=x_ctr+np.arange(0, 60, 120), DOPLOT=True) # defined in ATL06_to_ATL11  step=60
+#P11_list=fit_ATL11(filenames, beam_pair=3, seg_x_centers=x_ctr+np.arange(0, 60, 120), DOPLOT=None) # defined in ATL06_to_ATL11  step=60
+P11_list=fit_ATL11(filenames, beam_pair=3, seg_x_centers=None, DOPLOT=None, DEBUG=False) # defined in ATL06_to_ATL11  step=60
+D11=ATL11_data(len(P11_list), P11_list[0].N_reps).from_list(P11_list)
+D11.plot()
