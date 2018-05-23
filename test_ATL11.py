29106c273b875280cd464d171c3008792d8943fe
Ben Smith <bsmith@apl.washington.edu>
2018-05-23 10:12:21 -0700

    test_ATL11.py: changes to allow testing of the entire track

--- test_ATL11.py
+++ test_ATL11.py
@@ -8,6 +8,7 @@ from glob import glob
 import numpy as np
 from ATL06_to_ATL11 import fit_ATL11
 from ATL11 import ATL11_data
+from ATL11_plot import ATL11_plot
 #import sys
  
 np.seterr(invalid='ignore')
@@ -32,6 +33,7 @@ x_ctr=33047260
 # NEED TO TRY WITH THESE FILES AND PAIR=1
 
 #P11_list=fit_ATL11(filenames, beam_pair=3, seg_x_centers=x_ctr+np.arange(0, 60, 120), DOPLOT=None) # defined in ATL06_to_ATL11  step=60
-P11_list=fit_ATL11(filenames, beam_pair=3, seg_x_centers=None, DOPLOT=None, DEBUG=False) # defined in ATL06_to_ATL11  step=60
+#seg_x_centers=x_ctr+np.arange(-1200, 0, 60);
+P11_list=fit_ATL11(filenames, beam_pair=3, seg_x_centers=None , DOPLOT=None, DEBUG=False) # defined in ATL06_to_ATL11  step=60
 D11=ATL11_data(len(P11_list), P11_list[0].N_reps).from_list(P11_list)
-D11.plot()
+ATL11_plot(D11, P11_list)
