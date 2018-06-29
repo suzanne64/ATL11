021298879d20714f49e9fc5d6b3a3fee55eb0a17
Ben Smith <bsmith@apl.washington.edu>
2018-06-29 11:04:41 -0700

    ATL06_to_ATL11.py; changed iteration from along-track x to ref_pt_number, added mission_time_bounds parameter

--- ATL06_to_ATL11.py
+++ ATL06_to_ATL11.py
@@ -10,12 +10,16 @@ from mpl_toolkits.mplot3d import Axes3D
 
 #from poly_ref_surf import poly_ref_surf
 
-def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None, num_centers=None, DOPLOT=None, DEBUG=None, slope_change_t0=None):
+def fit_ATL11(ATL06_files, beam_pair=1, ref_pt_numbers=None, output_file=None, num_ref_pts=None, DOPLOT=None, DEBUG=None, mission_time_bds=None):
     params_11=ATL11_defaults()
+    seg_number_skip=int(params_11.seg_atc_spacing/20);
+    if mission_time_bds is None:
+        mission_time_bds=np.array([0, 365.25*3.*24.*3600.])
+
     
     # read in the ATL06 data from all the repeats
-    D6=ATL06_data(filename=ATL06_files, beam_pair=beam_pair, NICK=True) # two cols (two segs)
-
+    D6=ATL06_data(filename=ATL06_files, beam_pair=beam_pair, NICK=True)
+    
     # reoder data rows from D6 by cycle
     this_index=np.argsort(D6.cycle[:,0],axis=0)
     for field in D6.list_of_fields:
@@ -23,26 +27,22 @@ def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None, nu
         setattr(D6,field,tmp[this_index, :])
     
     P11_list=list()
-    if seg_x_centers is None:
+    if ref_pt_numbers is None:
         # NO: select every nth center        
-        seg_x_centers=np.arange(np.min(np.c_[D6.x_atc]), np.max(np.c_[D6.x_atc]), params_11.seg_atc_spacing) 
+        first_ref_pt=np.floor(np.nanmin(D6.segment_id.ravel())/seg_number_skip)*seg_number_skip
+        ref_pt_numbers=np.arange(first_ref_pt, np.max(D6.segment_id.ravel()), seg_number_skip) 
 
-    if num_centers is not None:
-        seg_x_centers=seg_x_centers[0:int(num_centers)]
-    for seg_x_center in seg_x_centers:
+    if num_ref_pts is not None:
+        ref_pt_numbers=ref_pt_numbers[0:int(num_ref_pts)]
+    for ref_pt_number in ref_pt_numbers:
+        x_atc_ctr=ref_pt_number*20.
         # section 5.1.1 
-        ref_pt_number=(np.floor((seg_x_center/20+params_11.N_search)/params_11.N_search)*params_11.N_search).astype(int)
         D6_sub=D6.subset(np.any(np.abs(D6.segment_id-ref_pt_number) <= params_11.N_search, axis=1), by_row=True)
-        #D6_sub=D6.subset(np.any(np.abs(D6.x_atc-seg_x_center) <= params_11.L_search_AT, axis=1), by_row=True)
-        
-        if DEBUG:
-            print('seg_x_center',seg_x_center)
-            print('D6 sub shape',D6_sub.x_atc.shape,D6_sub.sigma_geo_h.shape)
+   
         #2a. define representative x and y values for the pairs
         pair_data=D6_sub.get_pairs(datasets=['x_atc','y_atc','delta_time','dh_fit_dx','dh_fit_dy','segment_id','cycle','h_li'])   # this might go, similar to D6_sub
 
- 
-        P11=ATL11_point(N_pairs=len(pair_data.x), x_atc_ctr=seg_x_center, y_atc_ctr=None, track_azimuth=np.nanmedian(D6_sub.seg_azimuth.ravel()),N_reps=len(ATL06_files), slope_change_t0=slope_change_t0 )
+        P11=ATL11_point(N_pairs=len(pair_data.x), x_atc_ctr=x_atc_ctr, y_atc_ctr=None, track_azimuth=np.nanmedian(D6_sub.seg_azimuth.ravel()),N_reps=len(ATL06_files),  mission_time_bds=mission_time_bds )
         
         P11.DOPLOT=DOPLOT
        # step 2: select pairs, based on reasonable slopes
@@ -52,7 +52,7 @@ def fit_ATL11(ATL06_files, beam_pair=1, seg_x_centers=None, output_file=None, nu
             continue
         P11.select_y_center(D6_sub, pair_data)
                 
-        P11.corrected_h.ref_pt_lat,P11.corrected_h.ref_pt_lon = regress_to(D6_sub,['latitude','longitude'], ['x_atc','y_atc'],[seg_x_center,P11.y_atc_ctr])     
+        P11.corrected_h.ref_pt_lat,P11.corrected_h.ref_pt_lon = regress_to(D6_sub,['latitude','longitude'], ['x_atc','y_atc'],[x_atc_ctr,P11.y_atc_ctr])     
 
         P11.find_reference_surface(D6_sub)
 
