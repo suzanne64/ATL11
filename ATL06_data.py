d941055ae63d6bcc5189d9bb65fa96f545c1077d
Ben Smith <bsmith@apl.washington.edu>
2018-11-19 21:08:42 -0800

    ATL06_data.py: extensive changes to add documentation and bring the data format up to date

--- ATL06_data.py
+++ ATL06_data.py
@@ -11,79 +11,79 @@ import numpy as np
 from ATL11.ATL06_pair import ATL06_pair
 from osgeo import osr
 import matplotlib.pyplot as plt 
-import re
 
 class ATL06_data:
     np.seterr(invalid='ignore')
-    def __init__(self, filename=None, beam_pair=1, x_bounds=None, y_bounds=None, index_range=None, field_dict=None, list_of_fields=None, list_of_data=None, from_dict=None, NICK=None): 
+    def __init__(self, beam_pair=2, field_dict=None, list_of_fields=None, NICK=None): 
+        """
+        Initialize an ATl06 data structure. 
+        
+        ATL06_data has one private attribute:
+            __list_of_fields: list of all the fields defined for the current structure
+        and it has public attributes:
+            beam_pair:  The ICESat-2 beam pair from which the data come
+            size: the number of elements in each of its data fields
+            shape: the shape of each of its data fields
+        It has a variable number of data fields that are assigned during reading, 
+        each of which contains an  N x 2 numpy array of doubles that holds
+        data from the left and right beams of the ATL06 pair (gtxl and gtxr)
+        """
         if field_dict is None:
-            if NICK is not None:
-                field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'], 
-                            'ground_track':['cycle','x_atc', 'y_atc','seg_azimuth','sigma_geo_at','sigma_geo_xt'],
-                            'fit_statistics':['dh_fit_dx','dh_fit_dx_sigma','dh_fit_dy','h_rms_misft','h_robust_spread','signal_selection_source','snr_significance'],
-                            'geophysical':['bsnow_conf','bsnow_h','cloud_flg_asr','cloud_flg_atm','r_eff','tide_ocean']} 
-            else:
-                field_dict={None:['delta_time', 'h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id'], 
-                            'ground_track':['x_atc', 'y_atc','seg_azimuth'],
-                            'fit_statistics':['dh_fit_dx','dh_fit_dy','dh_fit_dx_sigma','h_robust_spread','snr_significance','signal_selection_source']} 
-
+            field_dict=self.__default_field_dict__()
+        
         if list_of_fields is None:
             list_of_fields=list()
             for group in field_dict.keys():
                 for field in field_dict[group]:
                     list_of_fields.append(field)
+                    
         self.beam_pair=beam_pair
-        self.list_of_fields=list_of_fields
-        if list_of_data is not None:
-            self.from_list(list_of_data)
-            return None     
-        if from_dict is not None:
-            self.list_of_fields=list_of_fields
-            for field in list_of_fields:
-                setattr(self, field, from_dict[field])
-            return
-        # read from a file if specified
-        if filename is not None:
-            # read a list of files if list provided
-            if isinstance(filename, (list, tuple)):
-                D6_list=[ATL06_data(filename=thisfile, field_dict=field_dict, beam_pair=beam_pair, index_range=index_range, x_bounds=x_bounds, y_bounds=y_bounds, NICK=NICK) for thisfile in filename]
-                self.from_list(D6_list)
-            elif isinstance(filename, (str)):
-                # this happens when the input filename is a string, not a list
-                self.read_from_file(filename, field_dict,  index_range=index_range, x_bounds=x_bounds, y_bounds=y_bounds, NICK=NICK)
-            else:
-                raise TypeError
-        else:
-            # no file specified, set blank
-            for field in list_of_fields:
-                setattr(self, field, np.zeros((2,0)))       
-          
-    def read_from_file(self, filename, field_dict, index_range=None, x_bounds=None, y_bounds=None, NICK=None): 
+        self.__list_of_fields=list_of_fields
+        for field in list_of_fields:
+            setattr(self, field, np.zeros((0,2)))   
+        self.size=0
+        self.shape=(0,2)
+        
+    def __update_size_and_shape__(self):
+        """
+        When data size and shape may have changed, update the size and shape atttributes
+        """
+        temp=getattr(self, self.__list_of_fields[0])
+        self.size=temp.size
+        self.shape=temp.shape
+        return self
+        
+    def __getitem__(self, rows):
+        """
+        Experimental: index the whole structure at once using square brackets
+        """
+        return self.copy().subset(rows)
+        
+    def __default_field_dict__(self):
+        """
+        Define the default fields that get read from the h5 file
+        """
+        field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'], 
+                            'ground_track':['x_atc', 'y_atc','seg_azimuth','sigma_geo_at','sigma_geo_xt'],
+                            'fit_statistics':['dh_fit_dx','dh_fit_dx_sigma','dh_fit_dy','h_rms_misfit','h_robust_sprd','signal_selection_source','snr_significance'],
+                            'geophysical':['bsnow_conf','bsnow_h','cloud_flg_asr','cloud_flg_atm','r_eff','tide_ocean']}         
+        return field_dict
+        
+    def from_file(self, filename, field_dict=None, index_range=None, x_bounds=None, y_bounds=None): 
+        """
+        Read data from a file.
+        """
         h5_f=h5py.File(filename,'r')        
+        if field_dict is None:
+            field_dict=self.__default_field_dict__()
+        
         beam_names=['gt%d%s' %(self.beam_pair, b) for b in ['l','r']]
         if beam_names[0] not in h5_f or beam_names[1] not in h5_f:        
             # return empty data structure
-            for group in field_dict:
+            for group in self.field_dict:
                 for field in group:
                     setattr(self, field, np.zeros((0,2)))
             return
-        # find cycle number in filename
-        if NICK:
-            m=re.search(r"TrackData_v._(.*?)/Track_(.*?).h5",filename)
-            if m is not None:
-                cycle_number=np.int(m.group(1))
-            else:
-                cycle_number=-1
-        else:
-            cycle_number=h5_f['ancillary_data']['start_cycle']            
-         #N.B. with real ATL06 data we will not want to apply the reference time offset to the delta time.
-        time_offset=0.
-        if NICK:
-            try:
-                time_offset=h5_f['metadata']['granule_start_seconds'][0]+(17.*365.+9*30.+15.)*24.*3600.
-            except KeyError:
-                time_offset=0.
-        #print('line 61,',NICK,filename)
         if beam_names[0] not in h5_f.keys():
             return None
         if index_range is None or index_range[1]==-1:
@@ -92,40 +92,53 @@ class ATL06_data:
         
         for group in field_dict.keys():
             for field in field_dict[group]:
-                if field not in self.list_of_fields:
-                    self.list_of_fields.append(field)
+                if field not in self.__list_of_fields:
+                    self.__list_of_fields.append(field)
                 try:
-                    if NICK is not None:
-                        if group is None:
-                            setattr(self, field, np.c_[
-                                np.array(h5_f[beam_names[0]]['land_ice_segments'][field][0,index_range[0]:index_range[1]]).transpose(),  
-                                np.array(h5_f[beam_names[1]]['land_ice_segments'][field][0,index_range[0]:index_range[1]]).transpose()])
-                        else:
-                            if 'ground' in group and 'cycle' in field:  # currently groundtrack/cycle NaN
-                                n_vals=np.array(h5_f[beam_names[0]]['land_ice_segments'][group][field][0,index_range[0]:index_range[1]]).size
-                                setattr(self, field, np.zeros( [n_vals, 2])+cycle_number)
-                            else:
-                                setattr(self, field, np.c_[
-                                np.array(h5_f[beam_names[0]]['land_ice_segments'][group][field][0,index_range[0]:index_range[1]]).transpose(),  
-                                np.array(h5_f[beam_names[1]]['land_ice_segments'][group][field][0,index_range[0]:index_range[1]]).transpose()])
+                    if group is None:
+                        #The None group corresponds to the top level of the land_ice_segments hirearchy
+                        # most datasets have a __fillValue attribute marking good data, but some don't
+                        try:
+                            bad_val=h5_f[beam_names[0]]['land_ice_segments'][field].attrs['_FillValue']
+                        except KeyError:
+                            #latitude and longitude don't have fill values, but -9999 works OK.
+                            bad_val=-9999
+                        data=np.c_[
+                            np.array(h5_f[beam_names[0]]['land_ice_segments'][field][index_range[0]:index_range[1]]).transpose(),  
+                            np.array(h5_f[beam_names[1]]['land_ice_segments'][field][index_range[0]:index_range[1]]).transpose()]
                     else:
-                        if group is None:
-                            setattr(self, field, np.c_[
-                                np.array(h5_f[beam_names[0]]['land_ice_segments'][field][index_range[0]:index_range[1]]).transpose(),  
-                                np.array(h5_f[beam_names[1]]['land_ice_segments'][field][index_range[0]:index_range[1]]).transpose()])
-                        else:
-                            setattr(self, field, np.c_[
-                                np.array(h5_f[beam_names[0]]['land_ice_segments'][group][field][index_range[0]:index_range[1]]).transpose(),  
-                                np.array(h5_f[beam_names[1]]['land_ice_segments'][group][field][index_range[0]:index_range[1]]).transpose()])                            
+                        # All other groups are under the land_ice_segments/group hirearchy
+                         try:
+                            bad_val=h5_f[beam_names[0]]['land_ice_segments'][group][field].attrs['_FillValue']
+                         except KeyError:
+                            #flags don't have fill values, but -9999 works OK.
+                            bad_val=-9999
+                         data=np.c_[
+                            np.array(h5_f[beam_names[0]]['land_ice_segments'][group][field][index_range[0]:index_range[1]]).transpose(),  
+                            np.array(h5_f[beam_names[1]]['land_ice_segments'][group][field][index_range[0]:index_range[1]]).transpose()]
+                    # identify the bad data elements before converting the field to double
+                    bad=data==bad_val
+                    data=data.astype(np.float64)
+                    # mark data that are invalid (according to the h5 file) with NaNs
+                    data[bad]=np.NaN
+                    setattr(self, field, data)                            
                 except KeyError:
                     print("could not read %s/%s" % (group, field))
                     setattr(self, field, np.zeros( [n_vals, 2])+np.NaN)
-        if NICK is not None:
-            self.sigma_geo_h =np.zeros_like(self.h_li)+0.03
-            self.sigma_geo_xt=np.zeros_like(self.h_li)+6.5
-            self.sigma_geo_at=np.zeros_like(self.h_li)+6.5
-            self.delta_time=self.delta_time*24.*3600.+time_offset
-        return
+        # add a non-broken version of the atl06 quality summary
+        setattr(self, 'atl06_quality_summary', (self.h_li_sigma > 1) | (self.h_robust_sprd > 1) | (self.snr_significance > 0.02))
+        h5_f.close()
+        self.__update_size_and_shape__()
+        # assign fields that must be copied from single-value attributes in the 
+        # h5 file
+        if 'cycle_number' in self.__list_of_fields:
+            # get the cycle number
+            try:
+                cycle_number=h5_f['ancillary_data']['start_cycle']  
+            except KeyError:
+                cycle_number=-9999
+            setattr(self, 'cycle_number', cycle_number+np.zeros(self.shape, dtype=np.float64))
+        return self
     
     def get_xy(self, proj4_string, EPSG=None):
         # method to get projected coordinates for the data.  Adds 'x' and 'y' fields to the data, optionally returns 'self'
@@ -144,56 +157,84 @@ class ATL06_data:
             x, y, z= list(zip(*[ct.TransformPoint(*xyz) for xyz in zip(np.ravel(self.longitude), np.ravel(self.latitude), np.zeros_like(np.ravel(self.latitude)))]))
             self.x=np.reshape(x, self.latitude.shape)
             self.y=np.reshape(y, self.longitude.shape)
-        if 'x' not in self.list_of_fields:
-            self.list_of_fields += ['x','y']
+        if 'x' not in self.__list_of_fields:
+            self.__list_of_fields += ['x','y']
         return self
     
     def append(self, D):
-        for field in self.list_of_fields:
+        """
+        Append the fields of two ATL06_data instances
+        """
+        for field in self.__list_of_fields:
             setattr(self, np.c_[getattr(self, field), getattr(D, field)])
-        return        
+        self.__update_size_and_shape__()    
+        return self
 
     def from_list(self, D6_list):
+        """
+        Append the fields of several ATL06_data instances.
+        """
         try:
-            for field in self.list_of_fields:
+            for field in self.__list_of_fields:
                 data_list=[getattr(this_D6, field) for this_D6 in D6_list]       
                 setattr(self, field, np.concatenate(data_list, 0))
         except TypeError:
-            for field in self.list_of_fields:
+            for field in self.__list_of_fields:
                 setattr(self, field, getattr(D6_list, field))
+        self.__update_size_and_shape__()
         return self
     
-    def index(self, index):
-        for field in self.list_of_fields:
-            setattr(self, field, getattr(self, field)[index,:])
-        return
+    def from_dict(self, D6_dict):
+        """
+        Build an ATL06_data from a dictionary
+        """
+        for field in self.__list_of_fields:
+            try:
+                setattr(self, field, D6_dict[field])
+            except KeyError:
+                setattr(self, field, None)
+        self.__update_size_and_shape__()
+        return self
+    
+    def index(self, rows):
+        """
+        Select a subset of rows within a given ATL06_data instance (modify in place)
+        """
+        for field in self.__list_of_fields:
+            setattr(self, field, getattr(self, field)[rows,:])
+        self.__update_size_and_shape__()
+        return self
         
     def subset(self, index, by_row=True, datasets=None):
+        """
+        Make a subsetted copy of the current ATL06_data instance
+        """
         dd=dict()
         if datasets is None:
-            datasets=self.list_of_fields
+            datasets=self.__list_of_fields
         for field in datasets:
             if by_row is not None and by_row:
                 dd[field]=getattr(self, field)[index,:]
             else:
                 dd[field]=getattr(self, field)[index,:].ravel()[index]
-        return ATL06_data(from_dict=dd, list_of_fields=datasets, beam_pair=self.beam_pair)
-            
+        return ATL06_data.from_dict(dd, list_of_fields=datasets, beam_pair=self.beam_pair)
+    
     def copy(self):
-        return ATL06_data(list_of_data=(self), list_of_fields=self.list_of_fields, beam_pair=self.beam_pair)
+        return ATL06_data( list_of_fields=self.__list_of_fields, beam_pair=self.beam_pair).from_list((self))
     
     def plot(self, valid_pairs=None, valid_segs=None):
         colors=('r','b')
         for col in (0, 1):
-            plt.errorbar(self.x_atc[:,col], self.h_li[:,col], yerr=self.h_li_sigma[:, col], c=colors[col], marker='.', linestyle='None', markersize=4);
+            #plt.errorbar(self.x_atc[:,col], self.h_li[:,col], yerr=self.h_li_sigma[:, col], c=colors[col], marker='.', linestyle='None', markersize=4);
+            plt.plot(self.x_atc[:,col], self.h_li[:,col], c=colors[col], marker='.', linestyle='None', markersize=2);
         if valid_segs is not None:
             for col in (0, 1):
-                plt.plot(self.x_atc[valid_segs[col], col], self.h_li[valid_segs[col], col],'marker','x',c=colors[col])
+                plt.plot(self.x_atc[valid_segs[col], col], self.h_li[valid_segs[col], col], marker='x',c=colors[col])
 
         if valid_pairs is not None:
             for col in (0, 1):
-                plt.plot(self.x_atc[valid_pairs, col], self.h_li[valid_pairs, col],'marker','o',c=colors[col])
-        plt.ylim(np.amin(self.h_li[self.atl06_quality_summary[:,1]==0,1])-5., np.amax(self.h_li[self.atl06_quality_summary[:,1]==0 ,1])+5 ) 
+                plt.plot(self.x_atc[valid_pairs, col], self.h_li[valid_pairs, col], marker='o',c=colors[col])
+        plt.ylim(np.nanmin(self.h_li[self.atl06_quality_summary[:,1]==0,1])-5., np.nanmax(self.h_li[self.atl06_quality_summary[:,1]==0 ,1])+5 ) 
         #plt.show()
         return
 
