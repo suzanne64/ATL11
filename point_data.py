2c50c386409d1016916c9a396f5c7a7a38f56985
Ben Smith <bsmith@apl.washington.edu>
2019-02-10 16:58:18 -0700

    point_data.py: extensive revisions

--- point_data.py
+++ point_data.py
@@ -7,21 +7,51 @@ Created on Fri Sep 21 14:28:30 2018
 import h5py
 import numpy as np
 from osgeo import osr
+import os
  
 
-class point_data:
+class point_data(object):
     np.seterr(invalid='ignore')
-    def __init__(self, list_of_fields=list(), SRS_proj4=None, columns=1):
+    def __init__(self, list_of_fields=None, SRS_proj4=None, field_dict=None, columns=0):
+        
+        if field_dict is None:
+            self.field_dict=self.__default_field_dict__()
+        else:
+            self.field_dict=field_dict
+            
+        if list_of_fields is None:
+            list_of_fields=list()
+            if field_dict is not None:
+                for group in self.field_dict.keys():
+                    for field in self.field_dict[group]:
+                        list_of_fields.append(field)
+        
         self.list_of_fields=list_of_fields
         self.SRS_proj4=SRS_proj4
-        self.columns=1
+        self.columns=columns
+        self.shape=None
 
+    def __default_field_dict__(self):
+        """
+        Define the default fields that get read from the h5 file
+        """
+        field_dict={None:('latitude','longitude','z')}
+        return field_dict
+        
     def copy_attrs(self):
         return point_data(list_of_fields=self.list_of_fields, SRS_proj4=self.SRS_proj4, columns=self.columns)
     
-    def from_file(self, filename, field_dict=None, index_range=(0, -1)):
+    def from_file(self, filename, field_dict=None, index_range=None):
         h5_f=h5py.File(filename, 'r')
         nan_fields=list()
+        if field_dict is None:
+            field_dict=self.field_dict
+        # make a slice out of whatever was provided in index_range
+        if index_range is None:
+            ind=slice(None)
+        else:
+            ind=slice(*index_range)
+            
         for group in field_dict.keys():
             for field in field_dict[group]:
                 if field not in self.list_of_fields:
@@ -29,14 +59,14 @@ class point_data:
                 try:
                     if group is None:
                         if self.columns==0 or self.columns is None:
-                            setattr(self, field, np.array(h5_f[field][index_range[0]:index_range[1]]).transpose())
+                            setattr(self, field, np.array(h5_f[field][ind]).transpose())
                         else:
-                            setattr(self, field, np.array(h5_f[field][:,index_range[0]:index_range[1]]).transpose())
+                            setattr(self, field, np.array(h5_f[field][:,ind]).transpose())
                     else:
                         if self.columns==0 or self.columns is None:
-                            setattr(self, field, np.array(h5_f[group][field][index_range[0]:index_range[1]]).transpose())  
+                            setattr(self, field, np.array(h5_f[group][field][ind]).transpose())  
                         else:
-                            setattr(self, field, np.array(h5_f[group][field][:,index_range[0]:index_range[1]]).transpose())  
+                            setattr(self, field, np.array(h5_f[group][field][:,ind]).transpose())  
                 except KeyError:
                     nan_fields.append(field)            
             # find the first populated field
@@ -52,16 +82,16 @@ class point_data:
         return self
 
     
-    def get_xy(self, proj4_str=None, EPSG=None):
+    def get_xy(self, proj4_string=None, EPSG=None):
+        # method to get projected coordinates for the data.  Adds 'x' and 'y' fields to the data, optionally returns 'self'
         out_srs=osr.SpatialReference()
-        if proj4_str is None and EPSG is not None:
+        if proj4_string is None and EPSG is not None:
             out_srs.ImportFromProj4(EPSG)
         else:
-            out_srs.ImportFromProj4(proj4_str)
+            out_srs.ImportFromProj4(proj4_string)
         ll_srs=osr.SpatialReference()
         ll_srs.ImportFromEPSG(4326)
         ct=osr.CoordinateTransformation(ll_srs, out_srs)
-        #x, y, z = list(zip(*[ct.TransformPoint(*xyz) for xyz in zip(np.ravel(D.longitude), np.ravel(D.latitude), np.zeros_like(np.ravel(D.latitude)))]))
         if self.latitude.size==0:
             self.x=np.zeros_like(self.latitude)
             self.y=np.zeros_like(self.latitude)
@@ -70,8 +100,7 @@ class point_data:
             self.x=np.reshape(x, self.latitude.shape)
             self.y=np.reshape(y, self.longitude.shape)
         if 'x' not in self.list_of_fields:
-            self.list_of_fields.append('x')
-            self.list_of_fields.append('y')
+            self.list_of_fields += ['x','y']
         return self
     
     def append(self, D):
@@ -120,3 +149,12 @@ class point_data:
                     dd[field]=temp_field.ravel()[index]
         return self.copy_attrs().from_dict(dd, list_of_fields=datasets)
 
+    def to_file(self, fileOut):
+        if os.path.isfile(fileOut):
+            os.remove(fileOut)
+        h5f_out=h5py.File(fileOut,'w')
+        for field in self.list_of_fields:
+            h5f_out.create_dataset(field,data=getattr(self,field))
+        h5f_out.close()
+        
+        
