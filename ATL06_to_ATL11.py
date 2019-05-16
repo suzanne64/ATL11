180edd340d29794375c5de04d36525651e7f546a
Ben Smith <bsmith@apl.washington.edu>
2019-05-16 09:48:26 -0600

    ATL06_to_ATL11.py: extensive revisions

--- ATL06_to_ATL11.py
+++ ATL06_to_ATL11.py
@@ -1,25 +1,23 @@
 import numpy as np
-from ATL06_data import ATL06_data
+from PointDatabase.ATL06_data import ATL06_data
 from ATL11_point import ATL11_point
 from ATL11_data import ATL11_data
-from ATL11_misc import ATL11_defaults
+from ATL11_misc import ATL11_defaults, default_ATL06_fields
 from glob import glob
 
 def fit_ATL11(ATL06_files, beam_pair=1, ref_pt_numbers=None, output_file=None, num_ref_pts=None, first_ref_pt=None, DOPLOT=None, DEBUG=None, mission_time_bds=None, verbose=False):
     params_11=ATL11_defaults()
     seg_number_skip=int(params_11.seg_atc_spacing/20);
     if mission_time_bds is None:
-        mission_time_bds=np.array([0, 365.25*3.*24.*3600.])+(17.*365.+9*30.+15.)*24.*3600
+        mission_time_bds=np.array([286.*24*3600, 398.*24*3600])
 
-    # read in the ATL06 data from all the repeats
-    D6=ATL06_data(filename=ATL06_files, beam_pair=beam_pair, NICK=True)
+    # read in the ATL06 data from all the repeats    
+    ATL06_field_dict=default_ATL06_fields()
+    D6=ATL06_data(beam_pair=beam_pair).from_list([ATL06_data(field_dict=ATL06_field_dict).from_file(filename) for filename in ATL06_files])
 
-    # reoder data rows from D6 by cycle
-    this_index=np.argsort(D6.cycle[:,0],axis=0)
-    for field in D6.list_of_fields:
-        tmp=getattr(D6,field)
-        setattr(D6,field,tmp[this_index, :])
-
+    # reorder data rows from D6 by cycle
+    D6.index(np.argsort(D6.cycle_number[:,0],axis=0))
+ 
     P11_list=list()
     if ref_pt_numbers is None:
         uId, iId=np.unique(D6.segment_id.ravel(), return_index=True)
@@ -40,49 +38,48 @@ def fit_ATL11(ATL06_files, beam_pair=1, ref_pt_numbers=None, output_file=None, n
 
     for count, ref_pt_number in enumerate(ref_pt_numbers):
         x_atc_ctr=ref_pt_x[count]
-        try:
-            # section 5.1.1
-            D6_sub=D6.subset(np.any(np.abs(D6.segment_id-ref_pt_number) <= params_11.N_search, axis=1), by_row=True)
-            if D6_sub.h_li.shape[0]<=1:
-                if verbose:
-                    print("not enough data at ref pt=%d" % ref_pt_number)
-                continue
+        # section 5.1.1
+        D6_sub=D6.subset(np.any(np.abs(D6.segment_id-ref_pt_number) <= params_11.N_search, axis=1), by_row=True)
+        if D6_sub.h_li.shape[0]<=1:
+            if verbose:
+                print("not enough data at ref pt=%d" % ref_pt_number)
+            continue
 
-            #2a. define representative x and y values for the pairs
-            pair_data=D6_sub.get_pairs(datasets=['x_atc','y_atc','delta_time','dh_fit_dx','dh_fit_dy','segment_id','cycle','h_li'])   # this might go, similar to D6_sub
-            P11=ATL11_point(N_pairs=len(pair_data.x), ref_pt_number=ref_pt_number, x_atc_ctr=x_atc_ctr, track_azimuth=np.nanmedian(D6_sub.seg_azimuth.ravel()),N_cycles=len(ATL06_files),  mission_time_bds=mission_time_bds )
+        #2a. define representative x and y values for the pairs
+        pair_data=D6_sub.get_pairs(datasets=['x_atc','y_atc','delta_time','dh_fit_dx','dh_fit_dy','segment_id','cycle_number','h_li'])   # this might go, similar to D6_sub
+        if ~np.any(np.isfinite(pair_data.y)):
+            continue
+        P11=ATL11_point(N_pairs=len(pair_data.x), ref_pt_number=ref_pt_number, x_atc_ctr=x_atc_ctr, track_azimuth=np.nanmedian(D6_sub.seg_azimuth.ravel()),N_cycles=len(ATL06_files),  mission_time_bds=mission_time_bds )
 
-            P11.DOPLOT=DOPLOT
-            # step 2: select pairs, based on reasonable slopes
-            P11.select_ATL06_pairs(D6_sub, pair_data)
-            if P11.ref_surf.surf_fit_quality_summary > 0:
-                P11_list.append(P11)
-                if verbose:
-                    print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
-                continue
-
-            P11.select_y_center(D6_sub, pair_data)
-            if P11.ref_surf.surf_fit_quality_summary > 0:
-                P11_list.append(P11)
-                if verbose:
-                    print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
-                continue
-
-            P11.corrected_h.ref_pt_lat,P11.corrected_h.ref_pt_lon = regress_to(D6_sub,['latitude','longitude'], ['x_atc','y_atc'],[x_atc_ctr,P11.y_atc_ctr])
-
-            P11.find_reference_surface(D6_sub)
-            if 'inversion failed' in P11.status:
-                P11_list.append(P11)
-                if verbose:
-                    print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
-
-                continue
-
-            P11.corr_heights_other_cycles(D6_sub)
-
+        P11.DOPLOT=DOPLOT
+        # step 2: select pairs, based on reasonable slopes
+        P11.select_ATL06_pairs(D6_sub, pair_data)
+        if P11.ref_surf.surf_fit_quality_summary > 0:
             P11_list.append(P11)
-        except:
-            print("uncaught exception for ref_pt=%d" % ref_pt_number)
+            if verbose:
+                print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
+            continue
+
+        P11.select_y_center(D6_sub, pair_data)
+        if P11.ref_surf.surf_fit_quality_summary > 0:
+            P11_list.append(P11)
+            if verbose:
+                print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
+            continue
+
+        P11.corrected_h.ref_pt_lat,P11.corrected_h.ref_pt_lon = regress_to(D6_sub,['latitude','longitude'], ['x_atc','y_atc'],[x_atc_ctr,P11.y_atc_ctr])
+
+        P11.find_reference_surface(D6_sub)
+        if 'inversion failed' in P11.status:
+            P11_list.append(P11)
+            if verbose:
+                print("surf_fit_quality=%d at ref pt=%d" % (P11.ref_surf.surf_fit_quality_summary, ref_pt_number))
+
+            continue
+
+        P11.corr_heights_other_cycles(D6_sub)
+
+        P11_list.append(P11)
         if np.mod(count, 1)==100:
             print("completed %d segments, ref_pt_number= %d" %(count, ref_pt_number))
 
@@ -116,7 +113,7 @@ def main():
     parser.add_argument('--out_file','-o', default=None, required=True)
     args=parser.parse_args()
     
-    ATL06_files=glob(args.ATL06_glob)
+    ATL06_files=glob(args.ATL06_glob.replace('"',''))
     if args.verbose:
         print("working on :")
         print(ATL06_files)
