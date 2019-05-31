4094c9a7ddca76bcc6f7e233e3e73414f05e84ec
Ben Smith <bsmith@apl.washington.edu>
2019-05-31 10:56:26 -0700

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#__all__=['ATL11_data','ATL11_misc','ATL11_plot','ATL11_point','poly_ref_surf']
+from .ATL11_misc import ATL11_defaults, default_ATL06_fields, poly_ref_surf
+from .ATL11_data import ATL11_data, valid_mask
+from .ATL11_point import ATL11_point
+#from .poly_ref_surf import poly_ref_surf
+
+
+
