54f1c13f6bc6ffba25c22cac01e1e431ab1116e5
smithB <bsmith@apl.washington.edu>
2019-08-13 18:00:33 -0400

    more refactoring

--- ATL06_to_ATL11.py
+++ ATL06_to_ATL11.py
@@ -38,53 +38,6 @@ def unwrap_lon(lon, lon0=0):
     lon[lon<-180] += 360
     return lon
 
-def get_xover_data(x0, y0, rgt, GI_file, xover_cache, index_bin_size, params_11):
-    """
-    Read the data from other tracks.
-
-    Maintain a cache of data so that subsequent reads don't have to reload data from disk
-    Inputs:
-        x0, y0: bin centers
-        rgt: current rgt
-        GI_file: geograpic index file
-        xover_cache: data cache (dict)
-        index_bin_size: size of the bins in the index
-        params_11: default parameter values for the ATL11 fit
-
-    """
-
-    # identify the crossover centers
-    x0_ctrs=buffered_bins(x0, y0, 2*params_11.L_search_XT, index_bin_size)
-    D_xover=[]
-
-    for x0_ctr in x0_ctrs:
-        this_key=(np.real(x0_ctr), np.imag(x0_ctr))
-        # check if we have already read in the data for this bin
-        if this_key not in xover_cache:
-            # if we haven't already read in the data, read it in.  These data will be in xover_cache[this_key]
-            xover_cache[this_key]={'D':point_data(field_dict=params_11.ATL06_field_dict).from_list(geo_index().from_file(GI_file).query_xy(this_key, fields=params_11.ATL06_field_dict))}
-            # remove the current rgt from data in the cache
-            xover_cache[this_key]['D'].index(~np.in1d(xover_cache[this_key]['D'].rgt, [rgt]))
-            if xover_cache[this_key]['D'].size==0:
-                continue
-            xover_cache[this_key]['D'].get_xy(EPSG=params_11.EPSG)
-            # index the cache at 100-m resolution
-            xover_cache[this_key]['index']=geo_index(delta=[100, 100], data=xover_cache[this_key]['D'])
-        # now read the data from the crossover cache
-        if xover_cache[this_key]['D'] is not None:
-            try:
-                Q=xover_cache[this_key]['index'].query_xy([x0, y0], pad=1, get_data=False)
-            except KeyError:
-                Q=None
-            if Q is None:
-                continue
-            # if we have read in any data for the current bin, subset it to the bins around the reference point
-            for key in Q:
-                for i0, i1 in zip(Q[key]['offset_start'], Q[key]['offset_end']):
-                    D_xover.append(xover_cache[this_key]['D'].subset(np.arange(i0, i1+1, dtype=int)))
-    if len(D_xover) > 0:
-        D_xover=point_data().from_list(D_xover)
-    return D_xover
 
 
 def regress_to(D, out_field_names, in_field_names, in_field_pt, DEBUG=None):
@@ -158,7 +111,7 @@ def main(argv):
         pairs=[args.pair]
     for pair in pairs:
         #P11_list=fit_ATL11(files, N_cycles=args.cycles,  beam_pair=pair, verbose=args.verbose, first_ref_pt=args.first_point, last_ref_pt=args.last_point) # defined in ATL06_to_ATL11
-        D11==ATL06_data().from_ATL06(files, N_cycles=args.cycles, beam_pair=pair, verbose=args.verbose, first_ref_pt=args.first_point, last_ref_pt=args.last_point, lonlat_bounds=args.bounds) # defined in ATL06_to_ATL11
+        D11=ATL06_data().from_ATL06(files, N_cycles=args.cycles, beam_pair=pair, verbose=args.verbose, first_ref_pt=args.first_point, last_ref_pt=args.last_point, lonlat_bounds=args.bounds) # defined in ATL06_to_ATL11
 
         D11.write_to_file(args.out_file)
 
