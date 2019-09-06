403fafd00b085d37e258cea3546160b06dd15ae9
Ben Smith <bsmith@apl.washington.edu>
2019-09-06 11:22:27 -0600

    misc.py: added hemisphere code, changed xover field list

--- misc.py
+++ misc.py
@@ -30,7 +30,7 @@ class defaults:
         self.equatorial_radius=6378137 # meters, on WGS84 spheroid
         self.polar_radius=6356752.3 # derived, https://www.eoas.ubc.ca/~mjelline/Planetary%20class/14gravity1_2.pdf
         self.ATL06_field_dict=default_ATL06_fields()
-
+        self.seg_number_skip=self.N_search
         # calculate the order for the polynomial degrees:  Sorted by degree, then by y degree, no sum of x and y degrees larger than max(degree_x, degree_y)
         degree_list_x, degree_list_y = np.meshgrid(np.arange(self.poly_max_degree_AT+1), np.arange(self.poly_max_degree_XT+1))
         # keep only degrees > 0 and degree_x+degree_y <= max(max_x_degree, max_y_degree)
@@ -44,6 +44,9 @@ class defaults:
         self.poly_exponent_list=np.transpose(np.vstack((degree_list_x[degree_order], degree_list_y[degree_order]))).tolist()
         self.poly_exponent={'x':degree_list_x, 'y':degree_list_y}
         self.N_coeffs=len(self.poly_exponent_list)
+        self.hemisphere=None
+        self.ATL06_xover_field_list=['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id', 'x_atc', 'dh_fit_dx', 'rgt','cycle_number', 'BP', 'LR', 'spot','sigma_geo_xt','sigma_geo_at']
+        
 
 def default_ATL06_fields():
     field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'],
