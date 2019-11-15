650eff779c1240958fb895aaecc0e8629fda2a80
Ben Smith <bsmith@apl.washington.edu>
2019-11-15 12:16:26 -0700

    group.py: made cycles a 2-element list

--- group.py
+++ group.py
@@ -14,10 +14,10 @@ class group(object):
     # some have one value for each point and each polynomial coefficient (poly_fields)
     # all of these are initialized to arrays of the appropriate size, filled with NaNs
 
-    def __init__(self, N_pts, N_cycles, N_coeffs, per_pt_fields=None, full_fields=None, poly_fields=None, xover_fields=None):
+    def __init__(self, N_pts, cycles, N_coeffs, per_pt_fields=None, full_fields=None, poly_fields=None, xover_fields=None):
         # input variables:
         #  N_pts: Number of reference points to allocate
-        #  N_cycles: Number of cycles of data to allocate
+        #  cycles: cycles for which to allocate data
         #  N_coeffs: Number of polynomial coefficients to allocate
         #  per_pt_fields: list of fields that have one value per reference point
         #  full_fields: list of fields that have one value per cycle per reference point
@@ -29,7 +29,7 @@ class group(object):
                 setattr(self, field, np.nan + np.zeros([N_pts, 1]))
         if full_fields is not None:
             for field in full_fields:
-                setattr(self, field, np.nan + np.zeros([N_pts, N_cycles]))
+                setattr(self, field, np.nan + np.zeros([N_pts, cycles[1]-cycles[0]+1]))
         if poly_fields is not None:
             for field in poly_fields:
                 setattr(self, field, np.nan + np.zeros([N_pts, N_coeffs]))
@@ -43,7 +43,8 @@ class group(object):
         self.poly_fields=poly_fields
         self.xover_fields=xover_fields
         self.list_of_fields=self.per_pt_fields+self.full_fields+self.poly_fields+self.xover_fields
-
+        self.attrs=dict()
+        
     def __repr__(self):
         out=''
         for field in self.list_of_fields:
