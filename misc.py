0c9a6fca98a354e49e2aea17e038d4c3926c5336
Ben Smith <bsmith@apl.washington.edu>
2019-11-15 12:13:00 -0700

    misc.py: changed maximum across-track degree to 3, changed across-track search to 65

--- misc.py
+++ misc.py
@@ -15,15 +15,13 @@ class defaults:
     def __init__(self):
         # provide option to read keyword=val pairs from the input file
         self.L_search_AT=100 # meters, along track (in x), filters along track
-        self.L_search_XT=110 # meters, cross track (in y), filters across track
-        #self.min_slope_tol=0.02 # in degrees?
-        #self.min_h_tol=0.1  # units? of height offset
+        self.L_search_XT=65 # meters, cross track (in y), filters across track
         self.seg_sigma_threshold_min=0.05
         self.beam_spacing=90 # meters
         self.seg_atc_spacing=100 # meters
         self.N_search=self.L_search_AT/20 # segments to include +/- from seg_atc_ctr in each ref_pt analysis
         self.poly_max_degree_AT=3
-        self.poly_max_degree_XT=3
+        self.poly_max_degree_XT=2
         self.xy_scale=100.         # meters
         self.t_scale=86400*365.25  # seconds/year.
         self.max_fit_iterations = 20  # maximum iterations when computing the reference surface models
@@ -45,15 +43,26 @@ class defaults:
         self.poly_exponent={'x':degree_list_x, 'y':degree_list_y}
         self.N_coeffs=len(self.poly_exponent_list)
         self.hemisphere=None
-        self.ATL06_xover_field_list=['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id', 'x_atc', 'dh_fit_dx', 'rgt','cycle_number', 'BP', 'LR', 'spot','sigma_geo_xt','sigma_geo_at']
+        self.ATL06_xover_field_list=['delta_time','h_li','h_li_sigma','latitude',\
+                                     'longitude','atl06_quality_summary','segment_id',\
+                                     'x_atc', 'dh_fit_dx', 'rgt','cycle_number',\
+                                     'BP', 'LR', 'spot','sigma_geo_xt','sigma_geo_at', \
+                                     'sigma_geo_h']
         
 
 def default_ATL06_fields():
-    field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'],
-                    'ground_track':['x_atc', 'y_atc','seg_azimuth','sigma_geo_at','sigma_geo_xt'],
-                    'fit_statistics':['dh_fit_dx','dh_fit_dx_sigma','h_mean', 'dh_fit_dy','h_rms_misfit','h_robust_sprd','n_fit_photons', 'signal_selection_source','snr_significance','w_surface_window_final'],
-                    'geophysical':['bsnow_conf','bsnow_h','cloud_flg_asr','cloud_flg_atm','r_eff','tide_ocean'],
+    field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude',
+                      'atl06_quality_summary','segment_id','sigma_geo_h'],
+                    'ground_track':['x_atc', 'y_atc','seg_azimuth','sigma_geo_at',
+                                    'sigma_geo_xt'],
+                    'fit_statistics':['dh_fit_dx','dh_fit_dx_sigma','h_mean', 
+                                      'dh_fit_dy','h_rms_misfit','h_robust_sprd',
+                                      'n_fit_photons', 'signal_selection_source',
+                                      'snr_significance','w_surface_window_final'],
+                    'geophysical':['bsnow_conf','bsnow_h','cloud_flg_asr',
+                                   'cloud_flg_atm','r_eff','tide_ocean'],
                     'orbit_info':['rgt','cycle_number'],
-                    'derived':['valid','BP', 'LR', 'spot', 'n_pixels', 'min_along_track_dh']}
+                    'derived':['valid','BP', 'LR', 'spot', 'n_pixels', 
+                               'min_along_track_dh']}
     return field_dict
 
