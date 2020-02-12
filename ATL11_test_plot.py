3560c34786eeaddf60b162fde83cabfe98314a03
Ben Smith <bsmith@apl.washington.edu>
2020-02-12 12:05:16 -0700

    ATL11_test_plot.py: added command line arguments, made code aware of the number of cycles present

--- ATL11_test_plot.py
+++ ATL11_test_plot.py
@@ -10,32 +10,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import sys
+import pointCollection as pc
 
-def ATL11_test_plot(ATL11_file):
+def ATL11_test_plot(ATL11_file, hemisphere=1, pair=2, mosaic=None):
     print(ATL11_file)
     #
     D = ATL11.data().from_file(ATL11_file, field_dict=None)
+    
+    if hemisphere==1:
+        D.get_xy(EPSG=3413)
+    else:
+        D.get_xy(EPSG=3031)
+      
     cm = matplotlib.cm.get_cmap('jet')
     colorslist = ['blue','red','orange','purple','brown','pink','gray','olive','cyan','black','yellow']
     ref, xo, delta = D.get_xovers()
 
     fig = plt.figure(1)
+    if mosaic is not None:
+        pc.grid.data().from_geotif(mosaic).show()
+        
     crange=[np.nanmin(xo.delta_time), np.nanmax(xo.delta_time)]
-    im = plt.scatter(D.corrected_h.longitude, D.corrected_h.latitude, c=D.corrected_h.delta_time[:,0], s=35, marker='.', cmap=cm, vmin=crange[0], vmax=crange[1])
-    im = plt.scatter(D.crossing_track_data.longitude, D.crossing_track_data.latitude, c=D.crossing_track_data.delta_time, s=35, marker='.', cmap=cm, vmin=crange[0], vmax=crange[1])
+    hl = plt.scatter(D.x, D.y, c=D.corrected_h.delta_time[:,0], s=35, marker='.', cmap=cm, vmin=crange[0], vmax=crange[1])
+    hl = plt.scatter(ref.x, ref.y, c=xo.delta_time, s=35, marker='x', cmap=cm, vmin=crange[0], vmax=crange[1])
     plt.title('Delta times of ATL11 data')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
-    fig.colorbar(im)
+    fig.colorbar(hl)
     
     
     fig = plt.figure(2)
-    for ii in [0,1]:
+    for ii in range(len(D.corrected_h.cycle_number)):
         plt.errorbar(D.ref_surf.x_atc, D.corrected_h.h_corr[:,ii], \
                 D.corrected_h.h_corr_sigma[:,ii] ,fmt='.',capsize=4,color=colorslist[ii])
-    for cycle in [3, 4]:
+    for cycle in D.corrected_h.cycle_number:
         ii=xo.cycle_number==cycle
-        #im = plt.errorbar(ref.x_atc[],ref.h_corr,ref.h_corr_sigma,fmt='.',capsize=4,color=colorslist[0]) 
         im = plt.errorbar(xo.x_atc[ii],xo.h_corr[ii],xo.h_corr_sigma[ii],fmt='x',capsize=4,color='k')
         ii=ref.cycle_number==cycle
         im = plt.errorbar(ref.x_atc[ii],ref.h_corr[ii],ref.h_corr_sigma[ii],fmt='*',capsize=4,color='g')
@@ -45,8 +54,11 @@ def ATL11_test_plot(ATL11_file):
     plt.ylabel('Heights [m]')
     
     fig = plt.figure(3)
-    plt.errorbar(D.ref_surf.x_atc, D.corrected_h.h_corr[:,1]-D.corrected_h.h_corr[:,0], \
-                np.sqrt(np.sum(D.corrected_h.h_corr_sigma**2, axis=1)) ,fmt='.',capsize=4,color=colorslist[0])
+    for col in range(1, len(D.corrected_h.cycle_number) ):
+        plt.errorbar(D.ref_surf.x_atc, 
+            D.corrected_h.h_corr[:,col]-D.corrected_h.h_corr[:,col-1], \
+            np.sqrt(np.sum(D.corrected_h.h_corr_sigma[:, col-1:col+1]**2, \
+                           axis=1)) ,fmt='.',capsize=4,color=colorslist[0])
     
     plt.title('Difference in Corrected Heights btn sequential cycles: later minus earlier')
     plt.xlabel('Along Track Distance [m]')
@@ -71,5 +83,12 @@ def ATL11_test_plot(ATL11_file):
     plt.show()
     
 if __name__=='__main__':
-    ATL11_test_plot(sys.argv[1])
+    import argparse
+    parser=argparse.ArgumentParser()
+    parser.add_argument('ATL11_file', type=str)
+    parser.add_argument('--Hemisphere','-H', type=int, default=1)
+    parser.add_argument('--pair','-p', type=int, default=2)
+    parser.add_argument('--mosaic', '-m', type=str)
+    args=parser.parse_args()
+    ATL11_test_plot(args.ATL11_file, pair=args.pair, hemisphere=args.Hemisphere, mosaic=args.mosaic)
     
